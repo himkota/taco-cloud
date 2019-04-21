fc5e08d5cda3970d1e5f19600ab3d05e4d593590
Himanshu Gupta <himanshu@Himanshus-MBP.fios-router.home>
2019-04-21 11:42:25 -0400

    order form

--- src/main/java/com/himanshu/tacos/controller/DesignTacoController.java
+++ src/main/java/com/himanshu/tacos/controller/DesignTacoController.java
@@ -55,5 +55,9 @@ public class DesignTacoController {
                 .collect(Collectors.toList());
     }
 	
-
+	@PostMapping
+	public String processDesign(Taco design) {
+		log.info("Processing design: " + design );
+		return "redirect:/orders/current";
+	}
 }
---
+++ src/main/java/com/himanshu/tacos/controller/OrderController.java
@@ -0,0 +1,23 @@
+package com.himanshu.tacos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.himanshu.tacos.model.Order;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+	
+	@GetMapping("/current")
+	public String orderForm(Model model) {
+		log.info("inside the order controller");
+		model.addAttribute("order", new Order());
+		return "orderForm";
+	}
+}
